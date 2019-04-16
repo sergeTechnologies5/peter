6bf7a85d53f3f86854f3de4a6a475b515b9bd02e
njerunjru <njesh74@gmail.com>
2019-04-16 19:56:42 +0300

    final

--- app/src/main/java/peter/retrofit/ApiClient.java
+++ app/src/main/java/peter/retrofit/ApiClient.java
@@ -13,7 +13,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.0.235:5000/api/";
+    private static final String BASE_URL = "http://192.168.0.26:5000/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
